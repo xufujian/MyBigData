d4ebacd7fdf810ca836043e835455fe38f0ffba6
xfjjs@live.com <xfjjs@live.com>
2021-07-20 22:47:04 +0800

    mypartition

---
+++ flink/src/main/java/com/stu/partition/partitionStu/MyPartition.java
@@ -0,0 +1,20 @@
+package com.stu.partition.partitionStu;
+
+import org.apache.flink.api.common.functions.Partitioner;
+
+/**
+ * @author: 今天风很大
+ * @date:2021/7/19 23:04
+ * @Description:
+ */
+public class MyPartition implements Partitioner<Long> {
+    @Override
+    public int partition(Long key, int numPartitions) {
+        System.out.println("分区总数:" + numPartitions);
+        if (key % 2 == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
